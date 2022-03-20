4dba9cb81d177670acc746648ac93f99d5e1f9c7
ParampreetR <raja.rai6789@gmail.com>
2022-03-20 23:30:48 +0530

    Improved AS information Component

--- src/components/Asc.js
+++ src/components/Asc.js
@@ -1,36 +1,28 @@
 export function AscInfo() {
   return (
-    <div>
-      <h1 class="text-center text-4xl my-4 underline underline-offset-8 , textfont-bold">
+    <div className="my-16 mx-4  md:mx-16 lg:mx-24">
+      <h1 className="text-center text-4xl my-4 text-[#141E27] font-bold">
         AS College, Khanna
       </h1>
-      <p class="text-center text-[#696969] my-1 text-bold">
+      <p className="text-center text-black opacity-50 my-1 text-bold text-lg">
         Re-credited with Grade "A" with CGPA of 3.51 out of 4 by NAAC
       </p>
 
-      <p class="text-2xl text-center text-[#00BFFF] italic my-8">
-        "The institution is committed to impart liberal education to all
-        sections
-        <br />
+      <p className="text-xl text-center text-primary italic my-8">
+        The institution is committed to impart liberal education to all sections
         of the society particularly the rural and the deprived ones, for shaping
-        a sterling character and scintillating carrer
-        <br />
-        imbibed with values of humanism, equality and secularism."
+        a sterling character and scintillating carrer imbibed with values of
+        humanism, equality and secularism.
       </p>
-      <p class="text-2xl text-center">
+      <p className="text-xl text-justify">
         A.S College is the great temple of learing since its inception in 1946,
-        the college has been trying its level best
-        <br />
-        to achieve varied goals and objectives as enshrined in constitution of
-        the institution. It has been shaping its
-        <br />
-        curiculor and administrative performance eeping pace with the emerging
-        trends in higher education. New courses
-        <br />
-        have been incorporated in its curricular framework from time to time to
-        fulfill the aspiration and needs of <br />
-        students. Many pass out students of this college are enjoying the
-        enviable positions in their fields.
+        the college has been trying its level best to achieve varied goals and
+        objectives as enshrined in constitution of the institution. It has been
+        shaping its curiculor and administrative performance eeping pace with
+        the emerging trends in higher education. New courses have been
+        incorporated in its curricular framework from time to time to fulfill
+        the aspiration and needs of students. Many pass out students of this
+        college are enjoying the enviable positions in their fields.
       </p>
     </div>
   );
