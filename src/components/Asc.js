77507e174031034dd394683b068adeb7daa99549
manroopsingh2666 <manroopsingh8707@gmail.com>
2022-03-20 21:47:52 +0530

    added as info component

--- src/components/Asc.js
+++ src/components/Asc.js
@@ -1,3 +1,37 @@
 export function AscInfo() {
-  return <div>All Code goes here</div>;
+  return (
+    <div>
+      <h1 class="text-center text-4xl my-4 underline underline-offset-8 , textfont-bold">
+        AS College, Khanna
+      </h1>
+      <p class="text-center text-[#696969] my-1 text-bold">
+        Re-credited with Grade "A" with CGPA of 3.51 out of 4 by NAAC
+      </p>
+
+      <p class="text-2xl text-center text-[#00BFFF] italic my-8">
+        "The institution is committed to impart liberal education to all
+        sections
+        <br />
+        of the society particularly the rural and the deprived ones, for shaping
+        a sterling character and scintillating carrer
+        <br />
+        imbibed with values of humanism, equality and secularism."
+      </p>
+      <p class="text-2xl text-center">
+        A.S College is the great temple of learing since its inception in 1946,
+        the college has been trying its level best
+        <br />
+        to achieve varied goals and objectives as enshrined in constitution of
+        the institution. It has been shaping its
+        <br />
+        curiculor and administrative performance eeping pace with the emerging
+        trends in higher education. New courses
+        <br />
+        have been incorporated in its curricular framework from time to time to
+        fulfill the aspiration and needs of <br />
+        students. Many pass out students of this college are enjoying the
+        enviable positions in their fields.
+      </p>
+    </div>
+  );
 }
