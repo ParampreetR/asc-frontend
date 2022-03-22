a1c0e9e2e81dae716bfaf8811d877e9eb6f83f55
ParampreetR <raja.rai6789@gmail.com>
2022-03-22 21:38:55 +0530

    Structured code and basic nav dropdown

---
+++ src/components/Notifications.js
@@ -0,0 +1,25 @@
+export function Notifications() {
+  return (
+    <div className="notifications">
+      <h2 className="text-3xl font-medium">Notifications</h2>
+      <div className="flex flex-col text-2xl w-screen mt-3">
+        <span className="my-2">
+          <span className="mx-20 my-auto w-80 inline-block">
+            Admission Open 2022-23
+          </span>
+          <button className="tranform duration-200 mx-20 transparent-btn text-xl px-5 py-2 font-medium hover:text-secondary2">
+            Apply Now
+          </button>
+        </span>
+        <span className="my-2">
+          <span className="mx-20 my-auto w-80 inline-block">
+            Notification 2
+          </span>
+          <button className="tranform duration-200 mx-20 transparent-btn text-xl px-5 py-2 font-medium hover:text-secondary2">
+            Button
+          </button>
+        </span>
+      </div>
+    </div>
+  );
+}
