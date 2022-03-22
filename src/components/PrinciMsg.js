2d8487b19e25b077a56c5350fe4547177b0baf2d
ParampreetR <raja.rai6789@gmail.com>
2022-03-22 22:01:34 +0530

    Fixed font and image in Principal Message component

--- src/components/PrinciMsg.js
+++ src/components/PrinciMsg.js
@@ -6,29 +6,31 @@ export function MsgFromPrinci() {
       </h1>
 
       <div className="text-center md:flex mt-5 md:mt-0">
-        <img
-          src="./principal_img.jpeg"
-          alt=""
-          className="w-4/5 mx-auto md:w-4/12 md:m-10"
-        />
-        <div className="md:my-auto">
-          <p className="text-justify px-8 mt-5 text-lg font-mono tracking-wider">
+        <div>
+          <img
+            src="./principal_img.jpeg"
+            alt=""
+            className="w-52 mx-auto md:w-96 md:m-10"
+          />
+        </div>
+        <div className="md:my-auto md:w-8/12">
+          <p className="px-8 mt-5 text-lg writing">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
             praesentium accusamus molestiae omnis officia perferendis libero!
             Nulla, eius, voluptatibus nesciunt doloribus pariatur voluptas
-            tenetur tempora minus veniam inventore earum quo. At,Lorem ipsum,
-            dolor sit amet consectetur adipisicing elit. Dolorem praesentium
-            accusamus molestiae omnis officia perferendis libero! Nulla, eius,
-            voluptatibus nesciunt doloribus pariatur voluptas tenetur tempora
-            minus veniam inventore earum quo. At quae. Tenetur blanditiis
-            explicabo officia nostrum qui exercitationem doloribus.
+            tenetur tempora minus veniam inventore earum quo.
           </p>
+          <p className="px-8 mt-5 text-lg writing">
+            At,Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+            praesentium accusamus molestiae omnis officia perferendis libero!
+            Nulla, eius, voluptatibus nesciunt doloribus pariatur voluptas
+            tenetur tempora minus veniam inventore earum quo. At quae. Tenetur
+            blanditiis explicabo officia nostrum qui exercitationem doloribus.
+          </p>
+          <button className="tranform duration-200 my-2 mb-7 mt-3 transparent-btn text-xl px-5 py-2 font-medium hover:text-secondary2">
+            Read More
+          </button>
         </div>
-      </div>
-      <div className="text-center">
-        <button className="tranform duration-200 my-2 mb-7 transparent-btn text-xl px-5 py-2 font-medium hover:text-secondary2">
-          Read More
-        </button>
       </div>
     </section>
   );
