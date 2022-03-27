6dced1fa3c14d2b149f214670d39e4dee990ee59
manroopsingh2666 <manroopsingh8707@gmail.com>
2022-03-27 14:40:39 +0530

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,40 @@
 export function Footer() {
   return (
     <section className="bg-[#EEEDDE] py-20">
-      <div className="mx-10 md:mx-14 lg:mx-20 xl:mx-32">Hello</div>
+      <div className="mx-10 md:mx-14 lg:mx-20 xxl:mx-32">
+        <div className="footer-container">
+          <div className="footer">
+            <div className="footer-heading footer-1">
+              <h1 className="text-xl">Quick Links</h1>
+              <a href="#"> Home</a>
+              <a href="#"> About Us</a>
+              <a href="#"> Admission</a>
+              <a href="#"> Courses</a>
+              <a href="#"> Fee-Details</a>
+              <a href="#"> Rules</a>
+            </div>
+            <div className="footer-heading footer-2">
+              <h1 className="text-xl">Discover</h1>
+              <a href="#"> Teaching</a>
+              <a href="#"> Activities</a>
+              <a href="#"> Events</a>
+              <a href="#"> Campus</a>
+              <a href="#"> Research</a>
+              <a href="#"> Consultancy</a>
+            </div>
+            <div className="footer-heading footer-3">
+              <h1 className="text-xl">Other Links</h1>
+              <a href="#"> Downloads</a>
+              <a href="#"> Forms</a>
+              <a href="#"> Jobs</a>
+              <a href="#"> IGNOU-Study</a>
+              <a href="#"> College-Magzine</a>
+              <a href="#"> Newsletter</a>
+              <img src="logo.jpg"></img>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
