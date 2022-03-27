969fa63853cd0b7bedfb68a8b894a5a827574a4a
ParampreetR <raja.rai6789@gmail.com>
2022-03-27 15:29:09 +0530

    Replaced a with Link in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export function Footer() {
   return (
     <section className="bg-[#EEEDDE] pt-20 pb-5">
@@ -7,45 +9,45 @@ export function Footer() {
             <h1 className="text-xl mb-1 font-medium text-primary">
               Quick Links
             </h1>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Home
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               About Us
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Admission
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Courses
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Fee-Details
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Rules
-            </a>
+            </Link>
           </div>
           <div className="flex flex-col mb-10">
             <h1 className="text-xl mb-1 font-medium text-primary">Discover</h1>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Teaching
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Activities
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Events
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Campus
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Research
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Consultancy
-            </a>
+            </Link>
           </div>
         </div>
         <div>
@@ -53,47 +55,47 @@ export function Footer() {
             <h1 className="text-xl mb-1 font-medium text-primary">
               Other Links
             </h1>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Downloads
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Forms
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Jobs
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               IGNOU-Study
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               College-Magzine
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Newsletter
-            </a>
+            </Link>
           </div>
           <div className="flex flex-col mb-10">
             <h1 className="text-xl mb-1 font-medium text-primary">
               Other Links
             </h1>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Downloads
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Forms
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Jobs
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               IGNOU-Study
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               College-Magzine
-            </a>
-            <a href="#" className="text-[#FE6D73] hover:text-[#a54548]">
+            </Link>
+            <Link to="#" className="text-[#FE6D73] hover:text-[#a54548]">
               Newsletter
-            </a>
+            </Link>
           </div>
         </div>
         <div className="my-auto pb-20">
