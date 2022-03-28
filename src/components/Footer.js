69f287e7ad72498a15babae93fa637cca54d9692
manroopsingh2666 <manroopsingh8707@gmail.com>
2022-03-28 16:38:51 +0530

    removed error in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,103 +1,153 @@
 export function Footer() {
-  return <div>
-    <footer class="bg-gray-200">
-      <div class="p-10 bg-gray-800 text-gray-200">
-        <div class="max-w-7xl mx-auto">
-          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
-            <div class="nb-5">
-              <h1 class="text-2xl pb-5">Quick Links</h1>
-              <ul class="text-gray-400">
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Home</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">About Us</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Admissions</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Courses</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Fee Details</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Rules</a>
-                </li>
-              </ul>
-            </div>
-            <div class="nb-5">
-              <h1 class="text-2xl pb-5">Discover</h1>
-              <ul class="text-gray-400">
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Teaching</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Activities</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Events</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Campus</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Research</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Consultancy</a>
-                </li>
-              </ul>
-            </div>
-            <div class="nb-5">
-              <h1 class="text-2xl pb-5">Other</h1>
-              <ul class="text-gray-400">
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Downloads</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Forms</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Jobs</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">IGNOU-Study</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">College Magznine</a>
-                </li>
-                <li class="pb-2">
-                  <a href="#" class="hover:text-yellow-500">Newsletter</a>
-                </li>
-              </ul>
-            </div>
-            <div class="nb-4">
-                <img class="pb-4" src="D:\images\logo.png" alt=""></img>
+  return (
+    <div>
+      <footer class="bg-gray-200">
+        <div class="p-10 bg-gray-800 text-gray-200">
+          <div class="max-w-7xl mx-auto">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+              <div class="nb-5">
+                <h1 class="text-2xl pb-5">Quick Links</h1>
+                <ul class="text-gray-400">
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Home
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      About Us
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Admissions
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Courses
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Fee Details
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Rules
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="nb-5">
+                <h1 class="text-2xl pb-5">Discover</h1>
+                <ul class="text-gray-400">
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Teaching
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Activities
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Events
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Campus
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Research
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Consultancy
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="nb-5">
+                <h1 class="text-2xl pb-5">Other</h1>
+                <ul class="text-gray-400">
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Downloads
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Forms
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Jobs
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      IGNOU-Study
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      College Magznine
+                    </a>
+                  </li>
+                  <li class="pb-2">
+                    <a href="#" class="hover:text-yellow-500">
+                      Newsletter
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="nb-4">
+                <img class="pb-4" src="logo.png" alt=""></img>
                 <h1 class="text-2xl pb-4">A.S College Khanna</h1>
-                 <p class="text-gray-400">
-                 Samrala Road, Machiwara Highway, </br>
-                 vill. Kalal Majra,  Khanna,
-                 </br> Punjab (141417).</br></br>
+                <p class="text-gray-400">
+                  Samrala Road, Machiwara Highway, <br />
+                  vill. Kalal Majra, Khanna,
+                  <br /> Punjab (141417).
+                  <br /> <br />
                 </p>
-            </div>
-          </div>
-        </div>
-    </div>
-        <div class="w-full bg-gray-900 text-gray-500 px-20">
-          <div>
-            <div class="text-center">
-              <div>
-               <strong> <span> Phone </span></strong> 091-01628-228470  <strong> <span> Email </span> </strong> asckhn@rediffmail.com 
-               <div> 
-                   Designed By <a href="#" class="text-yellow-500">Tailwind CSS</a>
-               </div>
               </div>
             </div>
           </div>
         </div>
-      
-    </footer>
-  </div>;
+        <div class="w-full bg-gray-900 text-gray-500 px-20">
+          <div>
+            <div class="text-center">
+              <div>
+                <strong>
+                  {" "}
+                  <span> Phone </span>
+                </strong>{" "}
+                091-01628-228470{" "}
+                <strong>
+                  {" "}
+                  <span> Email </span>{" "}
+                </strong>{" "}
+                asckhn@rediffmail.com
+                <div>
+                  Designed By{" "}
+                  <a href="#" class="text-yellow-500">
+                    Tailwind CSS
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 }
