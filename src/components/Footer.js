aa0412eb50af1beb5fa15c09dd3a0a27d2d62964
ParampreetR <raja.rai6789@gmail.com>
2022-03-28 16:53:13 +0530

    Improvements to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,32 +9,32 @@ export function Footer() {
                 <h1 class="text-2xl pb-5">Quick Links</h1>
                 <ul class="text-gray-400">
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Home
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       About Us
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Admissions
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Courses
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Fee Details
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Rules
                     </a>
                   </li>
@@ -44,32 +44,32 @@ export function Footer() {
                 <h1 class="text-2xl pb-5">Discover</h1>
                 <ul class="text-gray-400">
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Teaching
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Activities
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Events
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Campus
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Research
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Consultancy
                     </a>
                   </li>
@@ -79,46 +79,46 @@ export function Footer() {
                 <h1 class="text-2xl pb-5">Other</h1>
                 <ul class="text-gray-400">
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Downloads
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Forms
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Jobs
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       IGNOU-Study
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       College Magznine
                     </a>
                   </li>
                   <li class="pb-2">
-                    <a href="#" class="hover:text-yellow-500">
+                    <a href="#" class="hover:text-secondary2">
                       Newsletter
                     </a>
                   </li>
                 </ul>
               </div>
               <div class="nb-4">
-                <img class="pb-4" src="logo.png" alt=""></img>
+                <img class="pb-4" src="logo.png" alt="" />
                 <h1 class="text-2xl pb-4">A.S College Khanna</h1>
-                <p class="text-gray-400">
-                  Samrala Road, Machiwara Highway, <br />
-                  vill. Kalal Majra, Khanna,
-                  <br /> Punjab (141417).
+                <div class="text-gray-400">
+                  <h4>Samrala Road, Machiwara Highway,</h4>
+                  <h4>Vill. Kalal Majra, Khanna,</h4>
+                  <h4>Punjab (141417).</h4>
                   <br /> <br />
-                </p>
+                </div>
               </div>
             </div>
           </div>
@@ -126,23 +126,15 @@ export function Footer() {
         <div class="w-full bg-gray-900 text-gray-500 px-20">
           <div>
             <div class="text-center">
-              <div>
-                <strong>
-                  {" "}
-                  <span> Phone </span>
-                </strong>{" "}
-                091-01628-228470{" "}
-                <strong>
-                  {" "}
-                  <span> Email </span>{" "}
-                </strong>{" "}
-                asckhn@rediffmail.com
-                <div>
-                  Designed By{" "}
-                  <a href="#" class="text-yellow-500">
-                    Tailwind CSS
-                  </a>
-                </div>
+              <div className="flex flex-row justify-evenly">
+                <span>
+                  <span>Phone</span>
+                  091-01628-228470
+                </span>
+                <span>
+                  <span>Email</span>
+                  asckhn@rediffmail.com
+                </span>
               </div>
             </div>
           </div>
