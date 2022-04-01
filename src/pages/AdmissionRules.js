7abaad74b36efc5cb10ceacd8f09b3bde9689bff
manroopsingh2666 <manroopsingh8707@gmail.com>
2022-04-01 21:01:24 +0530

    added function to pages

--- src/pages/AdmissionRules.js
+++ src/pages/AdmissionRules.js
@@ -1,5 +1,5 @@
 ```js
-export function <Alumini>() {
+export function <AdmissionRules>() {
   return(
     <div className="my-16 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
---
+++ src/pages/GeneralRules.js
@@ -0,0 +1,21 @@
+```js
+export function <GeneralRules>() {
+  return(
+    <div className="my-16 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
+      <div className="max-w-[1440px] mx-auto">
+        <h1
+          className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold"
+        >
+          <!-- Headings -->
+        </h1>
+        <p className="text-center text-black opacity-50 my-1 text-md md:text-lg">
+          <!-- Paragraph -->
+        </p>
+        <p className="text-center text-black opacity-50 my-1 text-md md:text-lg">
+          <!-- Paragraph -->
+        </p>
+      </div>
+    </div>
+  );
+}
+```;
