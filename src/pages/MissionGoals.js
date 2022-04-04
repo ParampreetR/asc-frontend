a53b7fa4493ce71d7fbad5a613eb672b00714956
anonymous-dhyanesh <dhyanbawa12092002@gmail.com>
2022-04-04 19:44:29 +0530

    filled some text between predefined divs

--- src/pages/MissionGoals.js
+++ src/pages/MissionGoals.js
@@ -2,10 +2,14 @@ export function MissionGoals() {
   return (
     <div className="my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
-        <h1 className="text-center text-2xl md:text-4xl my-1.5 md:my-4 text-[#141E27] font-bold"></h1>
-        <p className="text-black  my-1 text-md md:text-lg tracking-wide"></p>
-        <p className="text-black  my-1 text-md md:text-lg tracking-wide"></p>
-      </div>
+        <h1 className="text-center text-2xl md:text-4xl my-1.5 md:my-4 text-[#141E27] font-bold">Mission & Goals.</h1>
+
+        
+        <p className="text-black  my-5 text-md md:text-lg tracking-wide">"The institution is committed to impart liberal holistic education to all sections of the society, particularly the rural and deprived ones, for shaping a sterling character and scintilltting career imbibed with values of humanism, equality and secularism."</p>
+
+        <h3 className="text-s md:text-4xl my-1.5 md:my-4 text-[#141E27] font-medium">Objectives of the Institution</h3>
+        <h4 className="text-s md:text-4xl my-1.5 md:my-4 text-[#141E27] font-medium">In consonance with its Vision and Mission, the college has adopted the following objectives :</h4>
+        </div>
     </div>
   );
 }
