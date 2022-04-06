ac5a33657bebc74815a1706216b444f7777e97f7
anonymous-dhyanesh <dhyanbawa12092002@gmail.com>
2022-04-06 20:46:23 +0530

    make page named Uniform Rules

--- src/pages/UniformRules.js
+++ src/pages/UniformRules.js
@@ -2,9 +2,28 @@ export function UniformRules() {
   return (
     <div className="my-16 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
-        {
-          // All webpage content here
-        }
+        
+          <h1 class="font-bold text-6xl text-center">College Uniform Rules</h1>
+
+          <h3 class="font-medium text-[30px] mt-[63px] -ml-20">For all girls of B.A. , B.Sc. , M.A. , M.Sc. :</h3>
+          <p class="max-w-4xl text-lg mt-2 -ml-2">
+            The College has prescribed for girls white shirt, white salwar, white doppatta, for summer and winter; and maroon cardigan(in winter only) as the College uniform. The wearing of it shall be compulsary on all <strong>Wednesdays, at College functions and on special occasions</strong>. Defaulters will be punished.
+          </p>
+
+          <h3 class="font-medium text-[30px] mt-[25px] -ml-20">For Students of B.C.A :</h3>
+          <h6 class="text-xl mt-[0.2rem] ">For Boys : </h6>
+          <p class="text-lg max-w=4xl ml-20 mt-1 ">Grey trousers, white shirts,  navy-blue turban, navy-blue tie and navy-blue  pullover in winter.</p>
+
+          <h6 class="text-xl mt-[0.2rem] ">For Girls : </h6>
+          <p class="text-lg max-w=4xl ml-20 mt-1">Grey suit, navy-grey Dupatta and navy-blue pullover in winter.</p>
+
+
+          <h3 class="font-medium text-[30px] mt-[25px] -ml-20">For Students of Foriegn Trade Practices and Procedures :</h3>
+          <h6 class="text-xl mt-[0.2rem] ">For Boys : </h6>
+          <p class="text-lg max-w=4xl ml-20 mt-1 ">White shirt, steel-grey trousers, tie navy-blue, naby-blue coat and black shoes.</p>
+
+          <h6 class="text-xl mt-[0.2rem] ">For Girls : </h6>
+          <p class="text-lg max-w=4xl ml-20 mt-1">White salwar suit, white dupatta and navy-blue cardigan in winter.</p>
       </div>
     </div>
   );
