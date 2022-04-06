0cc38c3070851cce9b544fb3676b097aa41ce289
anonymous-dhyanesh <dhyanbawa12092002@gmail.com>
2022-04-06 21:03:19 +0530

    replaced minus margins into positive

--- src/pages/UniformRules.js
+++ src/pages/UniformRules.js
@@ -5,12 +5,12 @@ export function UniformRules() {
         
           <h1 class="font-bold text-6xl text-center">College Uniform Rules</h1>
 
-          <h3 class="font-medium text-[30px] mt-[63px] -ml-20">For all girls of B.A. , B.Sc. , M.A. , M.Sc. :</h3>
+          <h3 class="font-medium text-[30px] mt-[63px] ml-20">For all girls of B.A. , B.Sc. , M.A. , M.Sc. :</h3>
           <p class="max-w-4xl text-lg mt-2 -ml-2">
             The College has prescribed for girls white shirt, white salwar, white doppatta, for summer and winter; and maroon cardigan(in winter only) as the College uniform. The wearing of it shall be compulsary on all <strong>Wednesdays, at College functions and on special occasions</strong>. Defaulters will be punished.
           </p>
 
-          <h3 class="font-medium text-[30px] mt-[25px] -ml-20">For Students of B.C.A :</h3>
+          <h3 class="font-medium text-[30px] mt-[25px] ml-20">For Students of B.C.A :</h3>
           <h6 class="text-xl mt-[0.2rem] ">For Boys : </h6>
           <p class="text-lg max-w=4xl ml-20 mt-1 ">Grey trousers, white shirts,  navy-blue turban, navy-blue tie and navy-blue  pullover in winter.</p>
 
@@ -18,7 +18,7 @@ export function UniformRules() {
           <p class="text-lg max-w=4xl ml-20 mt-1">Grey suit, navy-grey Dupatta and navy-blue pullover in winter.</p>
 
 
-          <h3 class="font-medium text-[30px] mt-[25px] -ml-20">For Students of Foriegn Trade Practices and Procedures :</h3>
+          <h3 class="font-medium text-[30px] mt-[25px] ml-20">For Students of Foriegn Trade Practices and Procedures :</h3>
           <h6 class="text-xl mt-[0.2rem] ">For Boys : </h6>
           <p class="text-lg max-w=4xl ml-20 mt-1 ">White shirt, steel-grey trousers, tie navy-blue, naby-blue coat and black shoes.</p>
 
