72eaed0270b7303520222a408af997f60ad81ea9
ParampreetR <raja.rai6789@gmail.com>
2022-04-07 18:29:16 +0530

    Some changes to Uniform Rules

--- src/pages/LeaveRules.js
+++ src/pages/LeaveRules.js
@@ -1,6 +1,6 @@
 export function LeaveRules() {
   return (
-    <div className="my-16 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
+    <div className="my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
         {
           // Leave rules page here
--- src/pages/UniformRules.js
+++ src/pages/UniformRules.js
@@ -1,27 +1,48 @@
 export function UniformRules() {
   return (
-    <div className="my-16 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
+    <div className="my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
-          <h1 class="font-bold text-2xl md:text-3xl text-center">College Uniform Rules</h1>
-          <h3 class="font-medium text-xl md:text-2xl mt-3">For all girls of B.A, B.Sc, M.A, M.Sc</h3>
-          <p class="max-w-4xl text-lg mt-2">
-            The College has prescribed for girls white shirt, white salwar, white doppatta, for summer and winter; and maroon cardigan(in winter only) as the College uniform. The wearing of it shall be compulsary on all <strong>Wednesdays, at College functions and on special occasions</strong>. Defaulters will be punished.
-          </p>
+        <h1 class="text-center text-2xl md:text-4xl my-1.5 md:my-4 text-[#141E27] font-bold">
+          College Uniform Rules
+        </h1>
+        <h2 class="text-2xl md:text-3xl my-2 md:mb-2 md:mt-9 text-[#141E27] font-medium">
+          For all girls of B.A, B.Sc, M.A, M.Sc
+        </h2>
+        <p class="max-w-4xl text-lg mt-2">
+          The College has prescribed for girls white shirt, white salwar, white
+          doppatta, for summer and winter; and maroon cardigan (in winter only)
+          as the College uniform. The wearing of it shall be compulsary on all
+          Wednesdays, at College functions and on special occasions. Defaulters
+          will be punished.
+        </p>
 
-          <h3 class="font-medium text-xl md:text-2xl mt-3">For Students of B.C.A</h3>
-          <h6 class="text-lg mt-2">For Boys</h6>
-          <p class="text-lg">Grey trousers, white shirts, navy-blue turban, navy-blue tie and navy-blue pullover in winter.</p>
+        <h2 class="text-2xl md:text-3xl my-2 md:mb-2 md:mt-9 text-[#141E27] font-medium3">
+          For Students of B.C.A
+        </h2>
+        <h3 class="text-lg mt-2">For Boys</h3>
+        <p class="text-lg">
+          Grey trousers, white shirts, navy-blue turban, navy-blue tie and
+          navy-blue pullover in winter.
+        </p>
 
-          <h6 class="text-xl mt-2 ">For Girls</h6>
-          <p class="text-lg mt-1">Grey suit, navy-grey Dupatta and navy-blue pullover in winter.</p>
+        <h3 class="text-xl mt-2 ">For Girls</h3>
+        <p class="text-lg mt-1">
+          Grey suit, navy-grey Dupatta and navy-blue pullover in winter.
+        </p>
 
+        <h2 class="text-2xl md:text-3xl my-2 md:mb-2 md:mt-9 text-[#141E27] font-medium3">
+          For Students of Foriegn Trade Practices and Procedures
+        </h2>
+        <h3 class="text-lg mt-2">For Boys</h3>
+        <p class="text-lg mt-1">
+          White shirt, steel-grey trousers, tie navy-blue, naby-blue coat and
+          black shoes.
+        </p>
 
-          <h4 class="font-medium text-xl md:text-2xl mt-3">For Students of Foriegn Trade Practices and Procedures :</h4>
-          <h5 class="text-lg mt-2">For Boys : </h5>
-          <p class="text-lg mt-1">White shirt, steel-grey trousers, tie navy-blue, naby-blue coat and black shoes.</p>
-
-          <h6 class="text-lg mt-2">For Girls : </h6>
-          <p class="text-lg mt-1">White salwar suit, white dupatta and navy-blue cardigan in winter.</p>
+        <h3 class="text-lg mt-2">For Girls</h3>
+        <p class="text-lg mt-1">
+          White salwar suit, white dupatta and navy-blue cardigan in winter.
+        </p>
       </div>
     </div>
   );
