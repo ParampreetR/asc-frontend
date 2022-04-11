09664b4920885fddc779636adc2f77a753e8b450
manroopsingh2666 <manroopsingh8707@gmail.com>
2022-04-11 21:49:55 +0530

    added list in history

--- src/pages/History.js
+++ src/pages/History.js
@@ -6,7 +6,7 @@ export function History() {
           About Us
         </h1>
         <p className="text-black  my-1 text-md md:text-lg tracking-wide">
-          The A.S. College, Khanna, was established in the year 1946. The
+          The <b>A.S. College, Khanna,</b> was established in the year 1946. The
           College is affiliated to Panjab University Chandigarh. It is a
           grant-in-aid institution. The college was established by the
           Anglo-Sanskrit High School Khanna Trust and Management Society whose
@@ -17,107 +17,108 @@ export function History() {
           The institution is recognized by the University Grants Commission
           (UGC) under Section 2(f) and 12(b). In view of the overall assessment
           of the institution, it has been accredited A+ Grade by NAAC, Bangalore
-          in 2004. In 2015, the institution was re-accredited by NAAC, Banglore
-          and has been awarded A grade with CGPA 3.51 out of 4. The college is
-          located in a semi-rural area on the out skirts of Khanna town. The
-          college offers B.A, B.Sc, B.Com, B.Com (E-Commerce), B.C.A and B.Voc
-          at undergraduate level and M.A in English, History, Pol. Science,
-          Mathematics,Punjabi, Economics at Post Graduate level. The college has
-          also started M.Sc(Chemistry) from session 2018-19. Besides this, the
-          college at present is running three certificate courses in Computer
-          Applications, Foreign Trade Practices & Procedures and Introduction to
-          Computer Sciences. More recently, UGC New Delhi has sanctioned Career
-          Oriented Programmes in Biotechnology, Industrial Chemistry, and Travel
-          & Tourism. Besides, a non-credit course on environmental education is
-          mandatory. The college also prepares its students to hone their skills
-          in spoken English and to appear in proficiency tests in the language
-          of English by arranging tutorials in IELTS and TOEFL. Basic courses in
-          German and French languages in non-formal mode are being offered by
-          the college. The college also offers crash courses in the use of
-          internet, Vermicomposting and Personality Development. The college
-          possesses a campus covering 21 acres of land. The unit cost of
-          education of the institution is more than Rs. 4642 approx. (excluding
-          salary component). The temporal plan for the academic work of the
-          college is of Semester type for all courses. The college has a Central
-          library, Computer Center, First-Aid facility, Canteen, Career Guidance
-          Centre, Staff Quarters for class IV employees. Besides, the college
-          has separate common rooms for boys and girls, separate cycle stand for
-          boys and girls and adequate parking space for employees. There are
-          adequate drinking water facilities and toilet blocks. The college now
-          has a total strength of 93 faculty members. Some of the staff members
-          of the college are on the Board of Studies, Syndicate, Senate and
-          Academic Council of the Panjab University. Within the framework of the
-          curriculum as prescribed by Panjab University, the college has been
-          developing such packages which enrich the knowledge, skill and values
-          that help students in achieving academic excellence. The teachers are
-          upgrading their knowledge and skills by regularly conducting and
-          participating in seminars, conferences, workshops, orientation courses
-          and refresher courses. The college has a strong Alumni Association
-          with a membership of over 1400 persons and quite a few of them occupy
-          prominent positions in public life. To name a few would be Sh. Chander
-          Shekhar, DGP Punjab; S. Pritpal Singh, ADGP, ITBP (President’s Medal);
-          Sh. D.S. Mangat, DGP (Retd.) Punjab Police; S. Lachhman Singh, PCS
-          (Judicial); Sh. Chanderjeet Singh, DCS (Judicial); Sh. Neeraj Goyal,
-          HCS (Judicial); Sh. Sanjay Kumar Gupta, IAS, Ministry of Finance; Late
-          Sh. Abhishek Chhatwal, IAS; S. Gursharan Singh Gill, ATEC Patiala; Sh.
-          Raman Goyal, IRS, Assistant Commissioner Income Tax Ludhiana; S.
-          Rajinder Singh, IRS; Mr. Shourie, HCS (Judicial); Sh. Boota Singh, IRS
-          Commissioner Income Tax, Mumbai; Sh. Pawan Kapil, President Reliance
-          Refineries, Jamnagar; Sh. S.S. Bari, Registrar Panjab University
-          Chandigarh; Sh. S.S. Kang, Former Chief Justice J & K High Court and
-          Former Governor of Karala; Sh. Jasjeet Randhawa, Former Cabinet
-          Minister Punjab; Sh. Bharpur Singh, Former Chairman PSEB; Sh. Shamsher
-          Singh Dullo, Former President PPCC; Dr. T.S. Kaler, world famous
-          cardiologist; S. Harpal Singh, Asian Medallist & Olympian; and a long
-          list continues.
+          in 2004.
+          <b>
+            In 2015, the institution was re-accredited by NAAC, Banglore and has
+            been awarded A grade with CGPA 3.51 out of 4.
+          </b>
+          The college is located in a semi-rural area on the out skirts of
+          Khanna town. The college offers B.A, B.Sc, B.Com, B.Com (E-Commerce),
+          B.C.A and B.Voc at undergraduate level and M.A in English, History,
+          Pol. Science, Mathematics,Punjabi, Economics at Post Graduate level.
+          The college has also started M.Sc(Chemistry) from session 2018-19.
+          Besides this, the college at present is running three certificate
+          courses in Computer Applications, Foreign Trade Practices & Procedures
+          and Introduction to Computer Sciences. More recently, UGC New Delhi
+          has sanctioned Career Oriented Programmes in Biotechnology, Industrial
+          Chemistry, and Travel & Tourism. Besides, a non-credit course on
+          environmental education is mandatory. The college also prepares its
+          students to hone their skills in spoken English and to appear in
+          proficiency tests in the language of English by arranging tutorials in
+          IELTS and TOEFL. Basic courses in German and French languages in
+          non-formal mode are being offered by the college. The college also
+          offers crash courses in the use of internet, Vermicomposting and
+          Personality Development. The college possesses a campus covering 21
+          acres of land. The unit cost of education of the institution is more
+          than Rs. 4642 approx. (excluding salary component). The temporal plan
+          for the academic work of the college is of Semester type for all
+          courses. The college has a Central library, Computer Center, First-Aid
+          facility, Canteen, Career Guidance Centre, Staff Quarters for class IV
+          employees. Besides, the college has separate common rooms for boys and
+          girls, separate cycle stand for boys and girls and adequate parking
+          space for employees. There are adequate drinking water facilities and
+          toilet blocks. The college now has a total strength of 93 faculty
+          members. Some of the staff members of the college are on the Board of
+          Studies, Syndicate, Senate and Academic Council of the Panjab
+          University. Within the framework of the curriculum as prescribed by
+          Panjab University, the college has been developing such packages which
+          enrich the knowledge, skill and values that help students in achieving
+          academic excellence. The teachers are upgrading their knowledge and
+          skills by regularly conducting and participating in seminars,
+          conferences, workshops, orientation courses and refresher courses. The
+          college has a strong Alumni Association with a membership of over 1400
+          persons and quite a few of them occupy prominent positions in public
+          life. To name a few would be Sh. Chander Shekhar, DGP Punjab; S.
+          Pritpal Singh, ADGP, ITBP (President’s Medal); Sh. D.S. Mangat, DGP
+          (Retd.) Punjab Police; S. Lachhman Singh, PCS (Judicial); Sh.
+          Chanderjeet Singh, DCS (Judicial); Sh. Neeraj Goyal, HCS (Judicial);
+          Sh. Sanjay Kumar Gupta, IAS, Ministry of Finance; Late Sh. Abhishek
+          Chhatwal, IAS; S. Gursharan Singh Gill, ATEC Patiala; Sh. Raman Goyal,
+          IRS, Assistant Commissioner Income Tax Ludhiana; S. Rajinder Singh,
+          IRS; Mr. Shourie, HCS (Judicial); Sh. Boota Singh, IRS Commissioner
+          Income Tax, Mumbai; Sh. Pawan Kapil, President Reliance Refineries,
+          Jamnagar; Sh. S.S. Bari, Registrar Panjab University Chandigarh; Sh.
+          S.S. Kang, Former Chief Justice J & K High Court and Former Governor
+          of Karala; Sh. Jasjeet Randhawa, Former Cabinet Minister Punjab; Sh.
+          Bharpur Singh, Former Chairman PSEB; Sh. Shamsher Singh Dullo, Former
+          President PPCC; Dr. T.S. Kaler, world famous cardiologist; S. Harpal
+          Singh, Asian Medallist & Olympian; and a long list continues.
         </p>
 
-        <h1 class="mt-3 ml-5 mr-5 font-bold ">
+        <h1 class="mt-3 ml-5 mr-5 font-bold text-xl ">
           {" "}
           The following activities being undertaken by the institution amply
           highlight the goals and objectivesof the institution:
         </h1>
-        <p className="text-black  my-1 text-md md:text-lg tracking-wide">
-          <p>
-            {" "}
-            1.To undertake new initiatives to improve infrastructuralfacilities,
+
+        <ul className="list-disc text-white  my-5 text-md md:text-lg bg-gradient-to-b from-primary to-primary2 rounded-md shadow-xl py-6 px-10 md:px-12 lg:px-16">
+          <li>
+            To undertake new initiatives to improve infrastructuralfacilities,
             examination system, library services etc.
-          </p>
-          <p>
-            {" "}
-            2.To organise and participate in extra-curricular activities such as
+          </li>
+
+          <li>
+            To organise and participate in extra-curricular activities such as
             NCC, NSS, talent hunt events, declamation contests,paper/poster
             presentation contests, debates, essay writing competitions,community
             services programmes, public awareness meets, sports events, and to
             commemorate days of special importance.
-          </p>
-          <p>
-            {" "}
-            3.To conduct and participate in seminars, workshops, conferences,
+          </li>
+
+          <li>
+            To conduct and participate in seminars, workshops, conferences,
             extension lectures, publish books & articles, provide research
             guidance, pusue research projects and develop linkage with alumni,
             professional societies, clubs, NGOs etc.
-          </p>
-          <p>
-            {" "}
-            4.To participate in different sports competitions/championships at
+          </li>
+
+          <li>
+            To participate in different sports competitions/championships at
             inter-college, university and inter-universitylevel.
-          </p>
-          <p>
-            {" "}
-            5.To participate in different items of the cultural and heritage
+          </li>
+
+          <li>
+            To participate in different items of the cultural and heritage
             festivals organised at zonal,university and inter-university level.
-          </p>
-          <p>
-            {" "}
-            6.To provide employment opportunities by arranging vocational
-            courses, crash courses, campus interviews and training sessions.
-          </p>
-          <p>
-            {" "}
-            7.To organise and participate in faculty enrichment programmes.
-          </p>
-        </p>
+          </li>
+
+          <li>
+            To provide employment opportunities by arranging vocational courses,
+            crash courses, campus interviews and training sessions.
+          </li>
+
+          <li>To organise and participate in faculty enrichment programmes.</li>
+        </ul>
       </div>
       <p class="text-xl text-center font-bold my-4 ml-5 mr-5">
         The college has evolved an institutional mechanism to monitor the
