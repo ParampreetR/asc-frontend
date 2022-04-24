441ab183f3e584cf19d0f7e52e6bc1911ca5a038
manroopsingh2666 <manroopsingh8707@gmail.com>
2022-04-24 23:08:57 +0530

    added library rules page

--- src/pages/LibraryRules.js
+++ src/pages/LibraryRules.js
@@ -2,9 +2,130 @@ export function LibraryRules() {
   return (
     <div className="my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
-        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold"></h1>
-        <p className="text-black my-1 text-md md:text-lg"></p>
-        <p className="text-black my-1 text-md md:text-lg"></p>
+        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold">
+          Library Rules
+        </h1>
+        <h2 class="text-2xl md:text-3xl my-2 md:mb-2 md:mt-9 text-[#141E27] dark:text-[#fefefe] font-medium">
+          About the Library
+        </h2>
+
+        <p className="text-white  text-white  my-5 text-md md:text-lg bg-gradient-to-b from-primary to-primary2 dark:from-primary_dark dark:to-primary2_dark rounded-md shadow-xl py-6 px-10 md:px-12 lg:px-16">
+          The Library is central and common to all the academic activities of
+          the College. It provides a place to study, material for study and
+          services to assist study. The college library has at present a good
+          collection of more than 50,000 books which include textbooks,
+          reference books, general books and non-book materials. The Library
+          collection is well classified, catalogued in a Systematic way. The
+          College Library is enriched with 30 Journals, 20 magazines and 12
+          Daily Newspapers. SOUL2.0 an integrated library software package is
+          installed for library automation.
+        </p>
+        <h2 class=" text-2xl md:text-3xl my-2 md:mb-2 md:mt-9 text-[#141E27] dark:text-[#fefefe] font-medium">
+          Library Timings
+        </h2>
+        <p className=" text-white  my-5 text-md md:text-lg bg-gradient-to-b from-primary to-primary2 dark:from-primary_dark dark:to-primary2_dark rounded-md shadow-xl py-6 px-10 md:px-12 lg:px-16">
+          Monday to Saturday: 8:45 a.m. to 4 p.m.
+        </p>
+        <h2 class="italic hover:not-italic text-2xl md:text-3xl my-2 md:mb-2 md:mt-9 text-[#141E27] dark:text-[#fefefe] font-medium font-italic">
+          The Library has been divided into different sections for its smooth
+          functioning:
+        </h2>
+        <ol
+          className="list-decimal text-xl text-white my-5 bg-gradient-to-b
+          from-primary to-primary2 dark:from-primary_dark dark:to-primary2_dark
+          rounded-md shadow-xl py-6 px-10 md:px-12 lg:px-16"
+        >
+          <li className=" font-bold">Circulation Section:</li>
+          <p class="text-lg">
+            This section has general and text books of all subjects. Library has
+            open access system. All the books have been arranged from 001 to 999
+            according to Dewey Decimal Classification System.
+          </p>
+
+          <li className=" font-bold">Reference Section:</li>
+          <p class="text-lg">
+            This section has important reference books on all subjects and
+            dictionaries, encyclopedia, thesaurus, travel guides, competitive
+            books, maps, gazetteers, collected works and Year Books.
+          </p>
+
+          <li className=" font-bold">Periodical Section:</li>
+          <p class="text-lg">
+            We subscribe more than 50 magazines/periodicals and 12 daily
+            newspapers in Hindi,English and Punjabi language received in the
+            reading room of the library.
+          </p>
+
+          <li className=" font-bold">Reading Room:</li>
+          <p class="text-lg">
+            The Reading Room has a capacity to accommodate 150 students at a
+            time.
+          </p>
+        </ol>
+        <h2 class="text-2xl md:text-4xl my-2 md:mb-2 md:mt-9 text-[#141E27] dark:text-[#fefefe] font-medium">
+          Services
+        </h2>
+        <ol
+          className="list-decimal text-xl text-white my-5 bg-gradient-to-b
+          from-primary to-primary2 dark:from-primary_dark dark:to-primary2_dark
+          rounded-md shadow-xl py-6 px-10 md:px-12 lg:px-16"
+        >
+          <li className=" font-bold">Issue and Return:</li>
+          <p class="text-lg">
+            This section is the hub of library activities where lending out of
+            books to readers is facilitated. The circulation of books for home
+            use is a major public service provided by the library. All the
+            members of the library visit this section for getting books on loan
+            for home use.
+          </p>
+
+          <li className=" font-bold">Issue and Return:</li>
+          <p class="text-lg">
+            Library provides book bank facility to needy and deserving students
+            for the whole session. The issued Text books have to be returned
+            after annual examination.
+          </p>
+
+          <li className=" font-bold">Reference Services:</li>
+          <p class="text-lg">
+            The function of libraries is three fold. Libraries acquire
+            information, organize that information in a way it can be retrieved
+            and disseminate the information the library has acquired. Reference
+            service fulfils this last function. Library provides assistance in
+            the use of library including location of material, use of computers
+            to access information and the use of basic reference sources.
+            Providing brief, factual answer to questions such as statistics,
+            address etc. that can be quickly located. Assistance in identifying
+            library material needed to answer a question.
+          </p>
+
+          <li className=" font-bold"> Internet Facility:</li>
+          <p class="text-lg">
+            U.G.C. NETWORK Resource Centre establishedin the Library.Eight
+            Computer Terminals have been placed for users for accessing
+            Internet. However, only educational sites are allowed for surfing.
+          </p>
+          <li className=" font-bold">Reprography Services:</li>
+          <p class="text-lg">
+            The reprographic facilities are available in Library for the
+            students and faculty members, at a very nominal rate.
+          </p>
+          <li className=" font-bold">Linkage with other Libraries:</li>
+          <p class="text-lg">
+            Though, we are having a best-resourced library, but we cannot offer
+            everything that every possible user may wish to access at one time.
+            For the purpose of fulfilling the requirements of our reader we have
+            provide membership of Panjab University Extension Library, Ludhiana
+            and A. C. Joshi Library, Panjab University, Chandigarh.
+          </p>
+          <li className=" font-bold">E- Resources:</li>
+          <p class="text-lg">
+            To provide access to e-resources to students and faculty members of
+            college, library also subscribedN-List Programme of INFLIBNET,
+            Gandhinagar. Under this more than 6000+e-journals and 97300+books
+            are available to the users of library.
+          </p>
+        </ol>
       </div>
     </div>
   );
