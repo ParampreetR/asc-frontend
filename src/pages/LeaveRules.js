2d90d816c19e71b16b1525aaa07c7678cd76ffc2
Parampreet Rai <59415797+ParampreetR@users.noreply.github.com>
2022-04-27 21:14:13 +0530

    Added dark mode support for Leave Rules

--- src/pages/LeaveRules.js
+++ src/pages/LeaveRules.js
@@ -2,7 +2,7 @@ export function LeaveRules() {
   return (
     <div className="my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
-        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold">
+        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold dark:text-[#dfdfdf]">
           Leave Rules
         </h1>
         <ul className="list-disc text-white  my-5 text-md md:text-lg bg-gradient-to-b from-primary to-primary2 dark:from-primary_dark dark:to-primary2_dark rounded-md shadow-xl py-6 px-10 md:px-12 lg:px-16">
