f038ebdfdeb2c99417d3192a1d2f65aa0647ac28
ParampreetR <raja.rai6789@gmail.com>
2022-05-03 10:50:28 +0530

    Improvements to Principal Page

--- src/pages/Principal.js
+++ src/pages/Principal.js
@@ -2,25 +2,32 @@ export function Principal() {
   return (
     <div className="my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
-        <h1 className="text-left  text-xl md:text-4xl my-2 md:my-4 text-[#141E27] dark:text-[#dfdfdf] ">
-          Dr. R.S Jhanji <br />
-          Principal A.S College Khanna
+        <h1 className="text-center font-bold text-xl md:text-4xl my-2 md:my-4 text-[#141E27] dark:text-[#dfdfdf]">
+          Principal of A.S College Khanna
         </h1>
-        <img
-          src="./Dr_ R_S_ Jhanji.jpg"
-          alt=""
-          className="object-none  object-right "
-        />
-        <h2 className="text-left  text-xl md:text-4xl my-2 md:my-4 text-[#141E27] dark:text-[#dfdfdf]">
+        <div className="text-center">
+          <span className="border-2 border-black dark:border-white inline-block shadow-xl">
+            <img
+              src="./principal_img.jpeg"
+              alt=""
+              className="m-auto w-[25rem]"
+            />
+            <h3 className="text-center text-2xl md:text-3xl font-medium my-2 md:my-4 text-[#141E27] dark:text-[#dfdfdf]">
+              Dr. R.S Jhanji
+            </h3>
+          </span>
+        </div>
+
+        <h2 className="text-xl md:text-2xl font-medium mt-2 mb-1 md:mt-4 md:mb-2 text-[#141E27] dark:text-[#dfdfdf]">
           Academic and Professional Journey
         </h2>
         <p className="text-black my-1 text-md md:text-lg dark:text-[#dfdfdf]">
           Born and raised in Rajpura town of Punjab, Dr. R.S. Jhanji did his
           schooling from prestigious Punjab Public School, Nabha and graduated
           in 1984 from S.A. Jain College, Ambala affiliated to Kurukshetra
-          University, Kurukshetra. After achieving Master’s Degree in English
-          from Punjabi University, Patiala in 1987, he researched on ‘Fiction
-          Works of Arun Joshi’ and completed his M.Phil. Degree in 1988. His
+          University, Kurukshetra. After achieving Master's Degree in English
+          from Punjabi University, Patiala in 1987, he researched on 'Fiction
+          Works of Arun Joshi' and completed his M.Phil. Degree in 1988. His
           initial stints spanning five years (1989-1994) in premier institutions
           in Punjab and Haryana provided him the fusion of professional training
           with University education, a step towards the fulfillment of
@@ -30,15 +37,16 @@ export function Principal() {
           handled the additional charge as the Officiating Principal of the
           college.
         </p>
-        <h2 className="text-left  text-xl md:text-4xl my-2 md:my-4 text-[#141E27] dark:text-[#dfdfdf]">
+
+        <h2 className=" text-xl md:text-2xl font-medium mt-2 mb-1 md:mt-4 md:mb-2 text-[#141E27] dark:text-[#dfdfdf]">
           Career and professional Journey at A.S College
         </h2>
         <p className="text-black my-1 text-md md:text-lg dark:text-[#dfdfdf] ">
           Consecutively, joined A.S. College, Khanna as a Lecturer in English in
           August 1997 and continued his research work in tandem with teaching
           work and other assignments and so earned his Doctorate Degree on
-          ‘Marginality versus Centre Paradigms of Colonial Experience in the
-          major Fiction of V.S. Naipaul’ in 2003 from Punjabi University,
+          'Marginality versus Centre Paradigms of Colonial Experience in the
+          major Fiction of V.S. Naipaul' in 2003 from Punjabi University,
           Patiala. On basis of his achievements and demonstrated success in
           teaching-learning, academics, research and corporate life of the
           institution, he was selected as the youngest ever Principal of this
@@ -69,7 +77,7 @@ export function Principal() {
           and Skill Development in Higher Education. He has attended many
           national and international conferences held in India and abroad.
         </p>
-        <h2 className="text-left  text-xl md:text-4xl my-2 md:my-4 text-[#141E27] dark:text-[#dfdfdf]">
+        <h2 className=" text-xl md:text-2xl font-medium mt-2 mb-1 md:mt-4 md:mb-2 text-[#141E27] dark:text-[#dfdfdf]">
           Role in Development and Additions in College
         </h2>
         <p className="text-black my-1 text-md md:text-lg dark:text-[#dfdfdf]">
@@ -77,9 +85,9 @@ export function Principal() {
           efforts for achieving the goals of infrastructural development,
           horizontal & vertical enrichment of academic programmes, research and
           faculty development and integration of technology in teaching-learning
-          process. In furtherance of ‘Innovative academic expansion’ the college
-          took a lead to respond to the UGC scheme on ‘Skill Development based
-          Higher Education’ notified by MHRD under National Skills
+          process. In furtherance of 'Innovative academic expansion' the college
+          took a lead to respond to the UGC scheme on 'Skill Development based
+          Higher Education' notified by MHRD under National Skills
           Qualifications Framework (NSQF). The college developed curriculum for
           vocational programmes of B.Voc- Multimedia: Animation & Graphics and
           B.Voc- Banking, Insurance & Retailing in consultation with industry
@@ -101,11 +109,11 @@ export function Principal() {
           opportunities and challenges of the institution with reference to
           Quality Sustenance and Enhancement. As a result of our joint efforts,
           scaling new heights and excellence in various academic and
-          co-curricular domains, A.S. College, Khanna has been awarded ‘Grade A’
+          co-curricular domains, A.S. College, Khanna has been awarded 'Grade A'
           with CGPA of 3.51on a 4 Point Scale by the National Assessment and
           Accreditation Council (NAAC).
         </p>
-        <h2 className="text-left  text-xl md:text-4xl my-2 md:my-4 text-[#141E27] dark:text-[#dfdfdf]">
+        <h2 className=" text-xl md:text-2xl font-medium mt-2 mb-1 md:mt-4 md:mb-2 text-[#141E27] dark:text-[#dfdfdf]">
           Other Honurs and Participations in Social and Professional Activities
         </h2>
         <p className="text-black my-1 text-md md:text-lg dark:text-[#dfdfdf]">
@@ -118,7 +126,7 @@ export function Principal() {
           Committee; Member, Joint Consultative Machinery; Member, College
           Development Council; Member, Academic Council; Member, Faculties of
           Science, Commerce, Arts; Chairperson, Panjab University Basket Ball
-          Selection Committee; Member, Board of Studies (01.04.17 – 31.03.2019);
+          Selection Committee; Member, Board of Studies (01.04.17 - 31.03.2019);
           Member, Committee for Implementation of National Digital Initiatives
           for the colleges in Punjab; Member, Examination Reforms Committee and
           Member, Youth Welfare Committee by the Vice-chancellor. As a member of
