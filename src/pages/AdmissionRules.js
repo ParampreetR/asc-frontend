f01b5c90451c9977c5af838148fd358022828778
manroopsingh2666 <manroopsingh8707@gmail.com>
2022-05-03 20:33:13 +0530

    changes in admission rules page

--- src/pages/AdmissionRules.js
+++ src/pages/AdmissionRules.js
@@ -2,7 +2,7 @@ export function AdmissionRules() {
   return (
     <div className="my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
-        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#303030] dark:text-white font-bold">
+        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#303030] dark:text-white ">
           Admission Rules
         </h1>
         <ol
@@ -10,38 +10,38 @@ export function AdmissionRules() {
           from-primary to-primary2 dark:from-primary_dark dark:to-primary2_dark
           rounded-md shadow-xl py-6 px-10 md:px-12 lg:px-16"
         >
-          <li className="font-bold mt-4">
+          <li className="mt-4">
             Failed/Compartment students from other institutions will not
             ordinarily be admitted.
           </li>
-          <li className="font-bold mt-4">
+          <li className="mt-4">
             A failed student who re-joins the as a regular student will be
             permitted to appear in the University/Board Examination as a regular
             student only if he fulfils the eligibility conditions in the year of
             re-joining.
           </li>
-          <li className="font-bold mt-4">
+          <li className="mt-4">
             Stagnant cases/candidates with a gap in their studies will not
             ordinarily be admitted. However, the gap may be condoned in the case
             of a student who for example had been on the rolls of this college
             during the previous academic session, paid all dues but could not
             appear in the University/Board Examination or had been detained.
           </li>
-          <li className="font-bold mt-4">
+          <li className="mt-4">
             Students selected for admission are required to deposit fees and
             other dues ON THE SPOT failing which they may have to forego their
             right to admission.
           </li>
-          <li className="font-bold mt-4">
+          <li className=" mt-4">
             Change of faculty or subject may be allowed strictly in accordance
             with University/Board rules.
           </li>
-          <li className="font-bold mt-4">
+          <li className=" mt-4">
             A candidate whose result is declared late by the Punjab University
             may join the college within 10 days of the declaration of his/her
             result.
           </li>
-          <li className="font-bold mt-4">
+          <li className=" mt-4">
             If a result of re-evaluation a candidate passes an examination or
             becomes eligible for the supplementary examination, he/she shall be
             eligible to seek admission to the next higher class within 10
@@ -51,59 +51,55 @@ export function AdmissionRules() {
             from-secondary to-primary2 dark:from-primary_dark dark:to-primary2_dark
             rounded-md shadow-xl py-6 px-10 md:px-12 lg:px-16"
             >
-              <li className="font-bold mt-4">
+              <li className="mt-4">
                 One attested copy of the University/Board certificate of the
                 Examination passed.
               </li>
-              <li className="font-bold mt-4">
-                Original University/Board certificate.
-              </li>
-              <li className="font-bold mt-4">
+              <li className="mt-4">Original University/Board certificate.</li>
+              <li className="mt-4">
                 Character certificate from the Head of the Institution last
                 attended.
               </li>
-              <li className="font-bold mt-4">
+              <li className="mt-4">
                 Caste Certificate from the S.D.M or class I Magistrate (for
                 Scheduled Caste students only).
               </li>
-              <li className="font-bold mt-4">
+              <li className="mt-4">
                 Migration and original certificate of the Examination passed in
                 the case of students coming from University/Board other than
                 Punjab.
               </li>
             </ul>
           </li>
-          <li className="font-bold mt-4">
+          <li className="mt-4">
             Students coming from Foreign countries must supply particulars of
             their passport, long term students Visa from Home Ministry and
             Eligibility Certificate from the Punjab University.{" "}
           </li>
-          <li className="font-bold mt-4">
+          <li className="mt-4">
             Admission to all classes is subject to the approval of Punjab
             University/P.S.E.B.{" "}
           </li>
-          <li className="font-bold mt-4">
+          <li className=" mt-4">
             Fine for absence without leave is :-
             <ul
               className="list-decimal text-xl text-[#303030] dark:text-white my-5 bg-gradient-to-b
             from-secondary to-primary2 dark:from-primary_dark dark:to-primary2_dark
             rounded-md shadow-xl py-6 px-10 md:px-12 lg:px-16"
             >
-              <li className="font-bold mt-4">For one period 50paisa</li>
-              <li className="font-bold mt-4">For one Practical Rs.1.</li>
-              <li className="font-bold mt-4">
-                For one Period composition Rs.1.
-              </li>
-              <li className="font-bold mt-4">For one Tutorial Rs.1.</li>
+              <li className="mt-4">For one period 50paisa</li>
+              <li className="mt-4">For one Practical Rs.1.</li>
+              <li className="mt-4">For one Period composition Rs.1.</li>
+              <li className="mt-4">For one Tutorial Rs.1.</li>
             </ul>
           </li>
-          <li className="font-bold mt-4">
+          <li className="mt-4">
             The name of a student who absents himself/herself for continuous
             period of seven days, without leave, shall be struck off the college
             rolls. He/She can seek re-admission after paying the arrears,
             re-admission fee and re-registration fee etc.{" "}
           </li>
-          <li className="font-bold mt-4">
+          <li className="mt-4">
             In case of any contradiction in the rules given in the prospectus
             with the University/Board rules, the latter shall have the
             over-riding effect.
