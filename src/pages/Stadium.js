b2ab56d473e393191b192dee2a60232bf330072e
manroopsingh2666 <manroopsingh8707@gmail.com>
2022-05-12 19:03:02 +0530

    added pages ugc and stadium

--- src/pages/Stadium.js
+++ src/pages/Stadium.js
@@ -2,9 +2,20 @@ export function Stadium() {
   return (
     <div className="my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
-        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold"></h1>
-        <p className="text-black my-1 text-md md:text-lg"></p>
-        <p className="text-black my-1 text-md md:text-lg"></p>
+        <img className="m-auto md-4" src="./stadium.jpeg" alt="" />
+        <br />
+        <h1 className="text-center text-2xl md:text-3xl mt-4 md-4 my-2 md:my-2 text-[#141E27] font-bold dark:text-[#dfdfdf]">
+          A.S College Stadium
+        </h1>
+        <p className="text-black my-1 text-md md:text-lg dark:text-[#dfdfdf] ">
+          MADAN GOPAL CHOPRA STADIUM OF A.S. COLLEGE IS ONE OF THE GREENEST
+          STADIUMS OF THE AREA. IT HAS BEEN CONSTRUCTED WITH THE HELP OF
+          GENEROUS CONTRIBUTIONS FROM THE COLLEGE ALUMNI, FACULTY, PARENTS AND
+          THE CITIZENS OF THE AREA. APART FROM COLLEGE SPORTS ACTIVITIES, THE
+          STADIUM IS ROUTINELY USED BY DAILY WALKERS AND VETERANS AND IT HAS
+          BEEN USED BY THE PUNJAB POLICE TIME TO TIME TO CONDUCT ITS RECRUITMENT
+          DRIVE. IT HAS ALSO BEEN USED TO ORGANIZE WORLD KABADDI CUP.
+        </p>
       </div>
     </div>
   );
--- src/pages/UGC.js
+++ src/pages/UGC.js
@@ -2,9 +2,25 @@ export function UGC() {
   return (
     <div className="my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
-        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold"></h1>
-        <p className="text-black my-1 text-md md:text-lg"></p>
-        <p className="text-black my-1 text-md md:text-lg"></p>
+        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold">
+          UGC Network Resource Centre
+        </h1>
+        <p className="text-black my-1 text-md md:text-lg">
+          University Grants Commission has entrusted the College to manage a
+          Network Resource Centre for providing updated information regarding
+          the activities, programmers and schemes of UGC to the students and
+          faculty members. The College has entrusted the responsibility to look
+          after this NRC to Department of Public Administration. The UGC (NRC)
+          is located in the Main Library of the College.
+        </p>
+        <p className="text-black my-1 text-md md:text-lg">
+          UGC Network Resource Centre provides free internet service to students
+          from 09.30 a.m to 04.00 pm. Besides this, the centre has arranged the
+          subject-wise list of websites to enable the students to prepare their
+          study notes. 50 to 70 students daily visit this centre and avails the
+          facility of internet. The library staff also helps the un-trained
+          students in surfing the internet.
+        </p>
       </div>
     </div>
   );
