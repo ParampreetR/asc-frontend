2f121c3061480ef12f16207973fba366dfa82438
ParampreetR <raja.rai6789@gmail.com>
2022-05-12 21:30:47 +0530

    Improvements to Seminar Hall page

--- src/pages/SeminarHall.js
+++ src/pages/SeminarHall.js
@@ -2,9 +2,19 @@ export function SeminarHall() {
   return (
     <div className="mt-18 my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
-        <h1 className=" text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold">Seminar Halls</h1>
-        <p className="text-black my-1 text-md md:text-lg mt-20">
-          College has two large  Seminar Halls with the seating capacity of 1000 people at a time. These are used to organize various curricular and co-curricular activities such as extension lectures, blood donation camps, cultural programmes, general assemblies and examinations.
+        <img
+          src="./seminar_hall.jpg"
+          alt=""
+          className="m-auto w-[50vw] max-w-[720px]"
+        />
+        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold dark:text-[#dfdfdf]">
+          Seminar Halls
+        </h1>
+        <p className="text-black my-1 text-md md:text-lg mt-2 dark:text-white">
+          College has two large Seminar Halls with the seating capacity of 1000
+          people at a time. These are used to organize various curricular and
+          co-curricular activities such as extension lectures, blood donation
+          camps, cultural programmes, general assemblies and examinations.
         </p>
       </div>
     </div>
