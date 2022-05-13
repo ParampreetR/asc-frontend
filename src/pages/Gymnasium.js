45ee81e334df3d350bf420d1646173e590b53b26
manroopsingh2666 <manroopsingh8707@gmail.com>
2022-05-13 23:01:33 +0530

    added auditorium and gymnasium page

--- src/pages/Gymnasium.js
+++ src/pages/Gymnasium.js
@@ -2,8 +2,101 @@ export function Gymnasium() {
   return (
     <div className="my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
-        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold"></h1>
-        <ul className=""></ul>
+        <h1 className="text-center text-2xl text-[#003060] md:text-4xl my-2 md:my-4 font-bold">
+          College Gymnasium
+        </h1>
+        <p className="text-black my-1 text-md md:text-lg mt-2 dark:text-white">
+          The College has various programs and facilities for students for
+          Sports activities and has always incouraged and supported students
+          about sports,fitness and good health.
+        </p>
+        <p className="text-black my-1 text-md md:text-lg mt-2 dark:text-white">
+          The College currently have following courts grounds and complexes for
+          fitness and sport activities:
+        </p>
+        <ul
+          className="list-disc text-white my-5 text-md md:text-lg 
+        bg-gradient-to-b from-primary to-primary2 dark:from-primary_dark dark:to-primary2_dark 
+        rounded-md shadow-xl py-6 px-10 md:px-12 lg:px-16"
+        >
+          <li>Outdoor GYM</li>
+          <li>Indoor GYM</li>
+          <li>Badminton Court</li>
+          <li>VolleyBall Court</li>
+          <li>BasketBall Court</li>
+          <li>Lawn Tennis Court</li>
+          <li>FootBall Ground</li>
+          <li>Hockey Ground</li>
+          <li>Cricket Ground</li>
+          <li>Boxing Cum Wrestling Ring</li>
+        </ul>
+        <h1 className="text-center text-2xl text-[#003060] mt-6 md:text-3xl my-2 md:my-4 text-[#141E27] font-bold">
+          Indoor GYM
+        </h1>
+        <p className="text-black text-center font-bold text-lg my-1 text-md md:text-lg mt-2 dark:text-white">
+          The college Indoor GYM has been recently renovated in 2022 and
+          equipped with modern fitness and exercise Machines
+        </p>
+        <img
+          src="./gym2.jpg"
+          alt=""
+          className=" mt-5 m-auto w-[50vw] max-w-[720px]"
+        />
+        <img
+          src="./gym3.jpg"
+          alt=""
+          className=" mt-5 m-auto w-[50vw] max-w-[720px]"
+        />
+        <img
+          src="./gym4.jpg"
+          alt=""
+          className=" mt-5 md-5 m-auto w-[50vw] max-w-[1080px]"
+        />
+        <h1 className="text-center text-2xl text-[#003060] md:text-3xl my-2 md:my-4 text-[#141E27] font-bold">
+          Badminton Court
+        </h1>
+        <p className="text-black text-center font-bold text-lg my-1 text-md md:text-lg mt-2 dark:text-white">
+          Badminton Wooden Court- Indoor badminton wooden court installed in the
+          college gymnasium hall. Fully approved material by BWF (Badminton
+          World Federation).
+        </p>
+        <img
+          src="./badminton1.jpg"
+          alt=""
+          className=" mt-5 m-auto w-[50vw] max-w-[720px]"
+        />
+        <img
+          src="./badminton2.jpeg"
+          alt=""
+          className=" mt-5 m-auto w-[50vw] max-w-[720px]"
+        />
+        <h1 className="text-center text-2xl text-[#003060] mt-6 md:text-3xl my-2 md:my-4 text-[#141E27] font-bold">
+          BasketBall Court
+        </h1>
+        <img
+          src="./basketballcourt.jpeg"
+          alt=""
+          className=" mt-5 m-auto w-[50vw] max-w-[720px]"
+        />
+        <h1 className="text-center text-2xl text-[#003060] mt-6 md:text-3xl my-2 md:my-4 text-[#141E27] font-bold">
+          Lawn Tennis Court
+        </h1>
+        <img
+          src="./Lawn_Tennis.jpg"
+          alt=""
+          className=" mt-5 m-auto w-[50vw] max-w-[720px]"
+        />
+        <p className="text-black text-center font-bold text-lg my-1 text-md md:text-lg mt-2 dark:text-white">
+          Lawn Tennis Court- Cemented Lawn Tennis court for practise.
+        </p>
+        <h1 className="text-center text-2xl text-[#003060] mt-6 md:text-3xl my-2 md:my-4 text-[#141E27] font-bold">
+          Cricket Ground
+        </h1>
+        <p className="text-black text-center font-bold text-lg my-1 text-md md:text-lg mt-2 dark:text-white">
+          Cricket Pitch- Cricket pitch constructed for practise, a cemented flat
+          pitch require minimal maintenance. College cricket players can
+          practice on pitch even after rain.
+        </p>
       </div>
     </div>
   );
