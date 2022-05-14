56db5321a2005364611d59b76e1509ad5920c45b
ParampreetR <raja.rai6789@gmail.com>
2022-05-14 21:15:46 +0530

    Added NSS page

--- src/pages/NSS.js
+++ src/pages/NSS.js
@@ -2,9 +2,159 @@ export function NSS() {
   return (
     <div className="my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
-        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold"></h1>
-        <p className="text-black my-1 text-md md:text-lg"></p>
-        <p className="text-black my-1 text-md md:text-lg"></p>
+        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] dark:text-[#dfdfdf] font-bold">
+          NSS
+        </h1>
+        <p className="text-black dark:text-white my-1 text-md md:text-lg">
+          NSS (National Service Scheme) unit enrols more than 250 Volunteers,
+          both boys and girls, every year. Major activities performed by NSS
+          Volunteers include
+        </p>
+        <ul className="list-disc text-white my-5 text-md md:text-lg bg-gradient-to-b from-primary to-primary2 dark:from-primary_dark dark:to-primary2_dark rounded-md shadow-xl py-6 px-10 md:px-12 lg:px-16">
+          <li>Tree Plantation/ Van Mahotsava 2</li>
+          <li>Blood Donation Camps 3</li>
+          <li>7 day NSS Camp 4</li>
+          <li>One Day Camps 5</li>
+          <li>Organisation of College functions etc</li>
+        </ul>
+
+        <p className="text-black dark:text-white my-1 text-md md:text-lg w-[60vw] dark:bg-secondary2_dark px-8 py-10 rounded-md m-auto">
+          <h2>Join NCC</h2>
+          <h4>Dear Students,</h4>
+          <p>
+            This is to inform you that the Registration as NSS volunteer is open
+            for the session 2021-22.
+          </p>
+          <h4>Instructions</h4>
+          <ul className="list-disc mt-5 mb-1 text-md md:text-lg py-6 px-5 md:px-6 lg:px-8">
+            <li>Online Registration is mandatory to join NSS</li>
+            <li>
+              Registration link is only for the bonafide students of
+              A.S.College, Khanna
+            </li>
+            <li>
+              Enrollment will be finalized by the Programme Officers, however
+              the weightage will be given to those students who are previously
+              involved with social activities
+            </li>
+            <li>
+              Participation in all the activities will be Compulsary for
+              Volunteers
+            </li>
+            <li>
+              The Registration link is open from 24-08-2021 to 30-09-2022. 6.
+              Volunteers will abide by all the rules and regulations of the
+              college and Punjab University,Chandigarh
+            </li>
+          </ul>
+          <a
+            className="underline hover:text-secondary2 font-medium text-center"
+            href="https://forms.gle/BCRRU1FuuUC5WoEJ6"
+          >
+            Registration link
+          </a>
+          <p>For Further details</p>
+          Contact
+          <br />
+          Programme officers ( Boys Unit )
+          <br />
+          Pr. Gagandeep Sethi
+          <br />
+          81460-00947
+          <br />
+          Pr.Arun Singla
+          <br />
+          95010 20972
+          <br />
+          <br />
+          Programme Officers (Girls Unit)
+          <br />
+          Pr. Monika Thakur
+          <br />
+          78377 33403
+          <br />
+          Pr.Jasdeep Kaur
+        </p>
+
+        <h2 className="text-2xl md:text-3xl my-2 md:mb-2 md:mt-9 text-[#141E27] dark:text-[#dfdfdf] font-bold">
+          NSS Activities 2021-2022
+        </h2>
+        <ul className="list-disc text-white  my-5 text-md md:text-lg bg-gradient-to-b from-primary to-primary2 dark:from-primary_dark dark:to-primary2_dark rounded-md shadow-xl py-6 px-10 md:px-12 lg:px-16">
+          <li>
+            Fifty volunteers participated in a two days cleanliness drive under
+            the theme Clean India on 18 October and 19 October 2021organised by
+            NSS Unit, A.S. College, Khanna.
+          </li>
+          <li>
+            A pledge taking ceremony was organized in the NSS Park to
+            commemorate National Unity Day on 30th October 2021. Fifty one
+            volunteers participated in the event.
+          </li>
+          <li>
+            A two days cleanliness drive was organized at college campus on 10th
+            and 11th December 2021. Seventy three volunteers participated in
+            this drive.
+          </li>
+          <li>
+            On 10th December 2021 a speech activity was held on the theme Voter
+            Awareness. Volunteers expressed their views on this said theme.
+          </li>
+          <li>
+            A debate competition was organized by NSS Unit College on the theme
+            Right to Vote is a Way to bring Good Governance. Six volunteers
+            participated and eighteen volunteers attended this event.
+          </li>
+          <li>
+            Fourteen volunteers participated in an essay writing competition on
+            the topic Local Unsung Heroes of the Freedom Struggle of India under
+            the ambit of Azadi Ka Amrit Mahotsava. Selected four essays were
+            forwarded on 10th January 2022 to NSS Department, Panjab University,
+            Chandigarh for participation in National Youth Festival 2022.
+          </li>
+          <li>
+            NSS Programme Officer Dr. Arun Singla and 10 volunteers donated
+            blood in the blood donation camp organized by Red Cross Society in
+            the memory of Sh. Krishan Murari Bedi on 11th January 2022.
+          </li>
+          <li>
+            Sixty volunteers registered and participated in National Youth
+            Festival 2022 held on 12 and 13 January 2022.
+          </li>
+          <li>
+            Sixty eight volunteers participated in an online webinar on National
+            Youth Day, 12 January 2022 organized by Department of Youth Welfare,
+            Panjab University, Chandigarh.
+          </li>
+          <li>
+            N.S.S Unit of A.S. College, Khanna participated in an online quiz
+            contest on the subject “Aatmanirbhar Bharat in Defence Production
+            and Apna Desh Apna Hathiyar” at the platform of MyGov during 5
+            January 2022 to 20 January 2022 to celebrate the Republic Day.
+            Eighty four volunteers participated in the quiz and after the
+            completion of quiz they got e-certificate.
+          </li>
+          <li>
+            Under the theme of Azadi Ka Amrit Mahotsava, ten volunteers
+            participated in an online Desh Bhakti Geet Writing and Patriotic
+            Lori Writing and two volunteers participated in Rangoli Making
+            Competitions and got e-certificates.
+          </li>
+        </ul>
+
+        <div className="text-center">
+          <img
+            src="./nss_blood_donation.jpg"
+            className="w-[70vw] max-w-[1080px] inline"
+          />
+          <img src="./nss1.jpg" className="w-[35vw] inline" />
+          <img src="./nss2.jpg" className="w-[35vw] inline" />
+          <img src="./nss3.jpg" className="w-[70vw] max-w-[1080px] inline" />
+          <img src="./nss4.jpg" className="w-[70vw] max-w-[1080px] inline" />
+          <img src="./nss5.jpg" className="w-[70vw] max-w-[1080px] inline" />
+          <img src="./nss6.jpg" className="w-[35vw] inline" />
+          <img src="./nss7.jpg" className="w-[35vw] inline" />
+          <img src="./nss8.jpg" className="w-[70vw] inline" />
+        </div>
       </div>
     </div>
   );
