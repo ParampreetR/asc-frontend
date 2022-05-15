9fbe6670f2f6fce16072c3f783fe4065080659ee
manroopsingh2666 <manroopsingh8707@gmail.com>
2022-05-15 15:04:19 +0530

    added rti page

--- src/pages/RTI.js
+++ src/pages/RTI.js
@@ -2,9 +2,55 @@ export function RTI() {
   return (
     <div className="my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
-        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold"></h1>
-        <p className="text-black my-1 text-md md:text-lg"></p>
-        <p className="text-black my-1 text-md md:text-lg"></p>
+        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#003060] font-bold">
+          RTI
+        </h1>
+        <h2 className="text-center mt-10 text-2xl md:text-2xl my-2 md:my-4 text-[#696969] font-bold">
+          Right To Information
+        </h2>
+        <p className="text-black dark:text-white my-1 text-md md:text-lg w-[60vw] shadow-xl bg-secondary2 dark:bg-secondary2_dark px-8 py-10 rounded-md m-auto">
+          <span
+            className="underline underline-offset-2 font-bold text-black my-1
+          text-md md:text-lg dark:text-white"
+          >
+            Name of the College:
+          </span>
+          &nbsp; &nbsp; A.S.College, Samrala Road, Khanna
+          <p className="text-black mt-5 my-1 text-md md:text-lg dark:text-white">
+            <span
+              className="underline underline-offset-2 font-bold text-black my-1
+             text-md md:text-lg dark:text-white"
+            >
+              Public Information Officer (PIO)
+            </span>
+            &nbsp; &nbsp; Sh. R.B.Yadav, Office Superintendent.
+          </p>
+          <p className="text-black mt-5 my-1 text-md md:text-lg dark:text-white">
+            <span
+              className="underline underline-offset-2 font-bold text-black my-1
+             text-md md:text-lg dark:text-white"
+            >
+              First Appellate Authority
+            </span>
+            &nbsp; &nbsp; Dr. R.S.Jhanji (Principal)
+          </p>
+          <p className="text-black mt-5 my-1 text-md md:text-lg dark:text-white">
+            <span
+              className="underline underline-offset-2 font-bold text-black my-1
+             text-md md:text-lg dark:text-white"
+            >
+              Address and Contact details
+            </span>
+            &nbsp; &nbsp; A.S.College, Khanna, Distt. Ludhiana. Phone:
+            01628-228470,225170 Email: asckhn@rediffmail.com &nbsp; &nbsp;
+            &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+            &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+            &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+            &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+            &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+            &nbsp;&nbsp;ascollegekhanna@gmail.com
+          </p>
+        </p>
       </div>
     </div>
   );
