0e8262b5ac63ab7540d59f9e8e6de7813507226e
manroopsingh2666 <manroopsingh8707@gmail.com>
2022-05-15 20:35:49 +0530

    changes in research and management pages

--- src/pages/Management.js
+++ src/pages/Management.js
@@ -2,11 +2,14 @@ export function Management() {
   return (
     <div className="my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
       <div className="max-w-[1440px] mx-auto">
-        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold">
+        <h1 className="text-center text-2xl md-7 md:text-4xl my-2 md:my-4 text-[#003060] dark:text-[#227C9D] font-bold">
           Organization and Management
         </h1>
       </div>
-      <img src="management-society.jpg" className="content-center"></img>
+      <img
+        src="management-society.jpg"
+        className=" mt-7 m-auto w-[50vw] max-w-[1080px]"
+      ></img>
     </div>
   );
 }
