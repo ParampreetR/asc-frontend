82a5d38fa23752f261054d9c7360fcb51bfea531
harsh <harsh@localhost.localdomain>
2022-05-15 21:43:02 +0300

    added two pages management and canteen

--- src/pages/Canteen.js
+++ src/pages/Canteen.js
@@ -1,11 +1,19 @@
 export function Canteen() {
   return (
     <div className="my-12 mx-8  md:mx-16 lg:mx-24 xl:mx-36">
-      <div className="max-w-[1440px] mx-auto">
-        <h1 className="text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold"></h1>
-        <p className="text-black my-1 text-md md:text-lg"></p>
-        <p className="text-black my-1 text-md md:text-lg"></p>
+      <div className="mx-auto">
+        <h1 className="mt-5 text-center text-2xl md:text-4xl my-2 md:my-4 text-[#141E27] font-bold">Canteen </h1>
+        <p className="text-black my-1 text-md md:text-lg mt-10 max-w-200">
+        A fully furnished canteen with separate sitting places for boys, girls and· staff is maintained by a private contractor. It serves hygienically prepared snacks, cold drinks and confectionery items on reasonable prices approved by the Canteen Committee of the college. 
+        </p>
+        
       </div>
+
+      <div className="inline-block max-w-100">
+        <img src="/canteen1.jpg" ></img>
+        <img src="/canteen2.jpg"></img>
+      </div>
+      
     </div>
   );
 }
